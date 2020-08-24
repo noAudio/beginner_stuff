5cc49cfe31139d37115ccdf22056edd643b3cc96
Samuel Odongo <samieodd@gmail.com>
2020-08-24 09:45:58 +0300

    simple gui console output

---
+++ exercises/simple_gui.py
@@ -0,0 +1,16 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for item in picture:
+    for sub_item in item:
+        if sub_item == 0:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print('')
